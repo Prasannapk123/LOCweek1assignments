3443702926d4443ddf99bb10c944a99a327a5c8e
Prasannapk123 <99386216+Prasannapk123@users.noreply.github.com>
2022-02-12 16:10:32 +0530

    Delete week 1 Q1.c

--- week 1 Q1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main ()
-{ 
- printf("NAME-Prasanna Kumar Sahu\nBRANCH-Mechanical Engineering\nRegd.no.-126\nMy hobbies are watching movies, playing games, spending time with friends.");
-return 0;
-    }
